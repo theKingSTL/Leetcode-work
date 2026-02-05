3d84240a9dd1aeb26776dd008d09598c1ffec1b8
STL <148905128+theKingSTL@users.noreply.github.com>
2026-02-05 12:35:24 -0600

    Time: 3 ms (51.92%), Space: 20.6 MB (23.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i == j:
-                    break
-                elif nums[i] + nums[j] == target:
-                    lis = [i,j]
-                    return lis
+        index = {}
+
+        for i, v in enumerate(nums):
+            #i = index, v = value in list 
+            if target - v in index:
+            #if target - current num return current num and index in set 
+                return [i, index[target-v]]
+            #if not return then add to dict i = index, v = key 
+            index[v] = i
