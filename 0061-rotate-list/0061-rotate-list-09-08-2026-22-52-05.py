66c87ace66978af14f860165ddb16b049d798c1f
STL <148905128+theKingSTL@users.noreply.github.com>
2026-09-08 22:52:06 -0700

    Time: 7 ms (2.48%), Space: 19.3 MB (40.46%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list-09-08-2026-22-52-05.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        node = head 
+        lenNode = head
+        if head == None:
+            return head
+        if head.next == None:
+            return head
+        length = 1
+        while lenNode.next != None:
+            lenNode = lenNode.next 
+            length = length + 1
+        newk = k%length 
+        if k == 0:
+            return head
+        for i in range(newk):
+            while node.next != None:
+                preNode = node
+                node = node.next
+            preNode.next = None
+            node.next = head 
+            head = node 
+            prevNode = head 
+        
+        return node
