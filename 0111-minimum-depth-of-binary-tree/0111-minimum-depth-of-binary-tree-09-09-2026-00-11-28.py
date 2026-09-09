59913ccb5b7073275199014574124a73c327ab17
STL <148905128+theKingSTL@users.noreply.github.com>
2026-09-09 00:11:29 -0700

    Time: 38 ms (42.47%), Space: 44.5 MB (75.48%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree-09-09-2026-00-11-28.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        def oneMore(n1, n2, d):
+            global depth
+            if not n1 and not n2:
+                depth = min(depth, d)
+                return
+            elif not n2:
+                oneMore(n1.left, n1.right, d+1)
+            elif not n1:
+                oneMore(n2.left, n2.right, d+1)
+            else:
+                oneMore(n2.left, n2.right, d+1)
+                oneMore(n1.left, n1.right, d+1)
+            return
+        global depth
+        passDepth = 1
+        depth = 10000
+        oneMore(root.left, root.right, passDepth)
+        return depth 
