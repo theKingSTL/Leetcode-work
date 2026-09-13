df1235303117047ffdde96c049de82108f256a1e
STL <148905128+theKingSTL@users.noreply.github.com>
2026-09-13 13:17:39 -0700

    Time: 89 ms (18.03%), Space: 35.3 MB (38.82%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii-09-13-2026-13-17-39.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        countOdds, minimum = 0, float('inf')
+        for i in range(len(nums1)):
+            minimum = min(minimum, nums1[i])
+            if nums1[i]%2 == 1:
+                countOdds +=1
+        if minimum%2 == 1 or countOdds == 0:
+            return True 
+        return False 
