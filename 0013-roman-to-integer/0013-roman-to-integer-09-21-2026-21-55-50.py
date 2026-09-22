7637146fcd9b347ebc39b922e24ecb07f9af3d6f
STL <148905128+theKingSTL@users.noreply.github.com>
2026-09-21 21:55:52 -0700

    Time: 7 ms (41.24%), Space: 19.3 MB (20.98%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer-09-21-2026-21-55-50.py
@@ -0,0 +1,8 @@
+rDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        for i, letter in enumerate(s[:-1]):
+            if rDict[letter] < rDict[s[i+1]] : res -= rDict[letter]
+            else: res += rDict[letter]
+        return res + rDict[s[-1]]
